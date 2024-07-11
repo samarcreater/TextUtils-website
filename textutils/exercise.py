5749a6bcb796a95ef42fe179e61e0b8e35d0923d
samarcreater <samarimp321@gmail.com>
2024-07-11 14:51:56 +0530

    Alphabets

---
+++ textutils/exercise.py
@@ -0,0 +1,7 @@
+# def ex1(request):
+#     sites = ['''<h1>For Entertainment </h1><a href = "https://www.youtube.com" >youtube videos</a>''',
+#              '''<h1>For Interaction </h1><a href = "https://www.facebook.com" >Facebook</a>''',
+#              '''<h1>For Insight </h1><a href = "https://www.ted.com/talks" >Ted Talk</a>''',
+#              '''<h1>For Internship </h1><a href = "https://www.internshala.com" >Internship</a>''',
+#              ]
+#     return HttpResponse((sites))
